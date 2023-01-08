cd40326827988fd4df02622a3bd42df74d1f915c
Arsenic99 <arsen09kz@gmail.com>
2023-01-08 17:44:45 +0500

    Added new component "Service"

---
+++ client/src/components/service.jsx
@@ -0,0 +1,29 @@
+import ServiceArrow from '../assets/images/ServiceArrow.svg'
+
+import { useState } from "react";
+const Service = (props) => {
+    const [webhover, setWebHover] = useState(false)
+    const [service, setService] = useState()
+        const hover = (e) => {
+            e.stopPropagation();
+            setWebHover(true);
+        }
+        const unhover = (e) => {
+            setWebHover(false);
+        }
+    return (
+        <div className='service' onMouseOver={(e) => hover(e)} onMouseOut={(e) => unhover(e)}>
+            {console.log(props)}
+            <h5>{props.service.title}</h5>
+            <p>{props.service.subtitle}</p>
+            <div className="d-flex justify-content-between">
+                <img src={webhover ? props.service.hoverImg : props.service.img} alt="ServiceWebComputer" />
+            </div>
+            <span style={{ display: webhover ? "block" : "none" }}>
+                <img src={ServiceArrow} alt="" />
+            </span>
+        </div>
+    );
+}
+
+export default Service;
--- client/src/components/services.jsx
+++ client/src/components/services.jsx
@@ -3,63 +3,20 @@ import ServiceWebComputerGray from '../assets/images/ServiceWebComputerGray.svg'
 import ServiceAdv from '../assets/images/ServiceAdv.png'
 import ServiceAdvGray from '../assets/images/ServiceAdvGray.png'
 import ServiceDesignGray from '../assets/images/ServiceDesignGray.png'
-import ServiceArrow from '../assets/images/ServiceArrow.svg'
 
-import { useState } from "react";
+import Service from './service'
 
 const Services = () => {
-    const [webhover, setWebHover] = useState(false)
-    const [service, setService] = useState()
-        const hover = (e) => {
-            e.stopPropagation();
-            setWebHover(true);
-            if(e.target.classList[0] === 'web'){
-                return setService(1)
-            }
-            if(e.target.classList[0] === 'adv'){
-                return setService(2)
-            }
-            if(e.target.classList[0] === 'design'){
-                return setService(3)
-            }
-        }
-        const unhover = (e) => {
-            setWebHover(false);
-        }
+    const services = [
+        {id:1, title: 'Сайт под ключ', subtitle: 'Разработаем современный сайт, с четким повествованием и привлекательным дизайном', img: ServiceWebComputerGray, hoverImg: ServiceWebComputer},
+        {id:2, title: 'Запуск рекламы', subtitle: 'Запустим контекстную/таргетированную рекламу, оптимизируем ее и создадим поток клиентов по лучшей цене', img: ServiceAdvGray, hoverImg: ServiceAdv},
+        {id:3, title: 'Графический дизайн', subtitle: 'Разработаем фирменный стиль, презентации, рекламные баннеры, шаблоны для постов и многое другое', img: ServiceDesignGray, hoverImg: ServiceDesignGray},
+    ]
     return (
         <section className="third-section">
             <h3>Услуги</h3>
             <div className="services">
-                <div className="web service" onMouseOver={(e) => hover(e)} onMouseOut={(e) => unhover(e)}>
-                    <h5>Сайт под ключ</h5>
-                    <p>Разработаем современный сайт, с четким повествованием и привлекательным дизайном</p>
-                    <div className="d-flex justify-content-between">
-                        <img src={webhover && service === 1 ? ServiceWebComputer : ServiceWebComputerGray} alt="ServiceWebComputer" />
-                    </div>
-                    <span style={{ display: webhover && service === 1 ? "block" : "none" }}>
-                        <img src={ServiceArrow} alt="" />
-                    </span>
-                </div>
-                <div className="adv service" onMouseOver={(e) => hover(e)} onMouseOut={(e) => unhover(e)}>
-                    <h5>Запуск рекламы</h5>
-                    <p>Запустим контекстную/таргетированную рекламу, оптимизируем ее и создадим поток клиентов по лучшей цене</p>
-                    <div className="d-flex justify-content-between">
-                        <img src={webhover && service === 2 ? ServiceAdv : ServiceAdvGray} alt="ServiceAdvGoogle" />
-                    </div>
-                    <span style={{ display: webhover && service === 2 ? "block" : "none" }}>
-                        <img src={ServiceArrow} alt="" />
-                    </span>
-                </div>
-                <div className="design service" onMouseOver={(e) => hover(e)} onMouseOut={(e) => unhover(e)}>
-                    <h5>Графический дизайн</h5>
-                    <p>Разработаем фирменный стиль, презентации, рекламные баннеры, шаблоны для постов и многое другое</p>
-                    <div className="service-design">
-                        <img src={webhover && service === 3 ? ServiceDesignGray : ServiceDesignGray} alt="ServiceDesign1" />
-                    </div>
-                    <span style={{ display: webhover && service === 3 ? "block" : "none" }}>
-                        <img src={ServiceArrow} alt="" />
-                    </span>
-                </div>
+                {services.map(item=><Service service = {item}/>)}
             </div>
         </section>
     );
